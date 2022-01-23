f59f98565358722d1e144a1b11ef95242b3128d3
Thomas-D40 <88280073+Thomas-D40@users.noreply.github.com>
2022-01-23 15:51:03 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+
+const Sauce = require("./models/stuff");
+const User = require("./models/user");
+
+const userRoutes = require("./routes/user");
+const stuffRoutes = require("./routes/stuff");
+
+const path = require("path");
+
+app.use(express.json());
+
+mongoose
+  .connect(
+    "mongodb+srv://tom:azer@cluster0.pjwiz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connection réussie"))
+  .catch(() => console.log("Echec de la connexion"));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  next();
+});
+
+app.use("/api/auth", userRoutes);
+app.use("/api/sauces", stuffRoutes);
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+module.exports = app;
---
+++ middleware/multer-config.js
@@ -0,0 +1,14 @@
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "images");
+  },
+  filename: (req, file, callback) => {
+    const name = file.originalname.split("").join("_");
+    const extension = file.mimetype.split("image/")[1];
+    callback(null, name + Date.now() + "." + extension);
+  },
+});
+
+module.exports = multer({ storage: storage }).single("image");
